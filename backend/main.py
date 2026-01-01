6c5021abe0867b4adf5aabb1b0182059ae680c4f
JangAyeon <ayeon3052@ewhain.net>
2026-01-02 00:42:19 +0900

    refactor: move project files to backend directory and add FastAPI chat endpoint

---
+++ backend/main.py
@@ -0,0 +1,83 @@
+from codecs import strict_errors
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain.messages import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
+
+import yfinance as yf
+
+load_dotenv() ## Load environment variables from .env file
+
+
+app = FastAPI()
+
+model = ChatOpenAI(
+    model = 'c1/openai/gpt-5/v-20250930',
+    base_url = 'https://api.thesys.dev/v1/embed/'
+)
+
+checkpoint = InMemorySaver()
+
+agent = create_agent(
+    model = model,
+    checkpoint = checkpoint,
+    tools = []
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+class PromptObject(BaseModel):
+    content:str
+    id:str
+    role:str
+
+
+class RequestObject(BaseModel):
+    prompt:PromptObject
+    threadId:str
+    responseId:str
+
+
+@app.post("/api/chat")
+async def chat(request: RequestObject):
+    config = {
+        "configurable":{
+            "thread_id":request.threadId,
+        }
+    }
+
+    def generate():
+        system_message = 'You are a stock analysis assistant. You have the ability to get real-time stock prices, historical stock prices (given a date range), news and balance sheet data for a given ticker symbol.'
+        for token, _ in agent.stream(
+            {"messages": [
+                    SystemMessage(content=system_message),
+                    HumanMessage(content=request.prompt.content),
+                ], },stream_mode="messsages",config=config
+        ):
+            yield token.content
+        return StreamingResponse(generate(), media_type="text/event-stream",
+            headers={"Cache-Control": "no-cache, no-transform", "Connection": "keep-alive"}
+        )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
